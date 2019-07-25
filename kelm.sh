47d42e66680272b843686d231fd5c9585a278f7a
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-07-25 01:44:57 +0000

    :wrench: remove export

--- kelm.sh
+++ kelm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eu
 
 initialize () {
-  export ENV_NAME=""
-  export DIRECTORY_NAME=""
-  export POD_NAME=""
-  export MSG_LEVEL=""
-  export MSG_TXT=""
-  export FLUG_ERROR=false
-  export FLUG_DEBUG=false
+  ENV_NAME=""
+  DIRECTORY_NAME=""
+  POD_NAME=""
+  MSG_LEVEL=""
+  MSG_TXT=""
+  FLUG_ERROR=false
+  FLUG_DEBUG=false
 }
 
 show_help () {
@@ -19,23 +19,23 @@ show_help () {
 # $2 = level [e:error, d:debug] OPTIONAL
 msg () {
   if [ $# -eq 0 ]; then
-    export MSG_TXT="message is empty : ${BASH_LINENO}"
-    export MSG_LEVEL="ERROR"
-    export FLUG_ERROR=true
+    MSG_TXT="message is empty : ${BASH_LINENO}"
+    MSG_LEVEL="ERROR"
+    FLUG_ERROR=true
   elif [ $# -ge 2 ]; then
     case $2 in
-      "e" ) export MSG_LEVEL="ERROR";;
+      "e" ) MSG_LEVEL="ERROR";;
       "d" ) 
         if [ $FLUG_DEBUG ]; then
-          export MSG_LEVEL="DEBUG"
+          MSG_LEVEL="DEBUG"
         else
           return 0
         fi ;;
     esac
-    export MSG_TXT=$1
+    MSG_TXT=$1
   else
-    export MSG_TXT=$1
-    export MSG_LEVEL="INFO"
+    MSG_TXT=$1
+    MSG_LEVEL="INFO"
   fi
   
   echo "[${MSG_LEVEL}] ${MSG_TXT}"
@@ -57,13 +57,13 @@ while getopts he:d:p: OPT
 do
   case $OPT in
     "e" )
-      export ENV_NAME=$OPTARG ;;
+      ENV_NAME=$OPTARG ;;
     "d" )
-      export DIRECTORY_NAME=$OPTARG ;;
+      DIRECTORY_NAME=$OPTARG ;;
     "p" )
-      export POD_NAME=$OPTARG ;;
+      POD_NAME=$OPTARG ;;
     "D" )
-      export FLUG_DEBUG=true ;;
+      FLUG_DEBUG=true ;;
     "h" | * | "" )
       show_help ;;
    esac

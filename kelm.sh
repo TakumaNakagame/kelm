1791ae7d2eb63d578196f0442f2c31072e5ad755
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-07-26 16:22:02 +0900

    :wrench: remove echo

--- kelm.sh
+++ kelm.sh
@@ -81,5 +81,5 @@ if [ -n "$ENV_NAME" ] && [ -n "$DIRECTORY_NAME" ] ; then
 fi
 
 if [ -n "$POD_NAME" ]; then
-  echo "kubectl delete pod --all-namespace -l app=${POD_NAME}"
+  kubectl delete pod --all-namespace -l app=${POD_NAME}
 fi
